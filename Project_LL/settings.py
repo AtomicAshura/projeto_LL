11f3042e6983d64d5a3cc6c1afd387bcf6da07c2
AtomicAshura <kaiomcpba@gmail.com>
2025-05-20 23:48:07 -0300

    Ajuste no settings para se conectar ao pythonanywhere

--- Project_LL/settings.py
+++ Project_LL/settings.py
@@ -9,9 +9,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!-#qmuf!xeo&^erkr7$jk#-s+*pxd8&%##47f*yer^q)o+50ti'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['dunas.pythonanywhere.com']
 
 
 # Application definition
